4ff4a4a86cea6cbaef1c1ec06c5affadf3dad895
spiritualjesus <108991087+spiritualjesus@users.noreply.github.com>
2022-07-10 11:44:43 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,9 +6,9 @@ $company = "Gourmet Mushroom Co";
 /* Database credentials. Please edit */
 
 define('DB_SERVER', 'localhost'); 
-define('DB_USERNAME', 'db_user');
-define('DB_PASSWORD', 'db_pass');
-define('DB_NAME', 'db_name');
+define('DB_USERNAME', 'db_user_edit');
+define('DB_PASSWORD', 'db_pass_edit');
+define('DB_NAME', 'db_name_edit');
 
 /* DO NOT EDIT BELOW THIS LINE */
 $db2 = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
